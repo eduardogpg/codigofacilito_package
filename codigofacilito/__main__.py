7ba9e4c452a0efed421d4d9f344effcc7b7b9985
eduardogpg <eduardo78d@gmail.com>
2022-01-12 17:59:26 -0600

    Final commity

--- codigofacilito/__main__.py
+++ codigofacilito/__main__.py
@@ -10,12 +10,13 @@ ERROR -> 40
 CRITICAL -> 50
 """
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.INFO)
 
+# Ejecuta todo lo del bloque sí y solo sí, este archivo se ejecuta como principal.
 if __name__ == '__main__':
     logging.debug('>>> Estamos comenzando la ejecución del paquete.')
     
     workshops = unreleased()
-    logging.debug(workshops)
+    # logging.debug(help(unreleased))
     
     logging.debug('>>> Estamos finalizando la ejecución del paquete.')
--- codigofacilito/workshops.py
+++ codigofacilito/workshops.py
@@ -1,8 +1,13 @@
 import requests
 
 def unreleased():
+    """Retorna los próximos talleres en CódigoFacilito.
+    
+    >>> type(unreleased()) == type(dict())
+    True
+    """
     response = requests.get('https://codigofacilito.com/api/v2/workshops/unreleased')
     
     if response.status_code == 200:
-        payload = response.json()    
+        payload = response.json() 
         return payload['data']
